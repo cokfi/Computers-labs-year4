48e32c67d1a4f176a8e6e14b456bb34c501fd531
cokfi <74899649+cokfi@users.noreply.github.com>
2022-06-27 16:12:48 +0300

    pipe1

--- Lab5/VHDL/MIPS.vhd
+++ Lab5/VHDL/MIPS.vhd
@@ -15,7 +15,15 @@
 --
 -- Revision: 
 -- Revision 0.01 - File Created
--- Additional Comments: 
+--
+-- Additional Comments:
+-- *** There are four stages of pipe : ***
+-- Fetch 	(instruction fetch)
+-- Decode 	(decode instruction, read registers from registr file and generate control lines)
+-- Execute 	(arithmetic operations by ALU)
+-- Memory 	(write/read data from memory)
+-- WB 		(write back to register file)
+-- *** signals will have prefix according to their stage *** 
 --
 ----------------------------------------------------------------------------------				
 LIBRARY IEEE;
@@ -48,9 +56,18 @@ ARCHITECTURE structure OF MIPS IS
 -----------------------------------------------
 -- signals declaration
 -----------------------------------------------
+-- *** There are four stages of pipe : ***
+-- Fetch 	(instruction fetch)
+-- Decode 	(decode instruction, read registers from registr file and generate control lines)
+-- Execute 	(arithmetic operations by ALU)
+-- Memory 	(write/read data from memory)
+-- WB 		(write back to register file)
+-- *** signals will have prefix according to their stage ***
 
 	SIGNAL CLKout			: STD_LOGIC; -- with period 2*T_clock , the divider's output
-	SIGNAL PC_plus_4 		: STD_LOGIC_VECTOR( 9 DOWNTO 0 );
+	SIGNAL FetchPC_plus_4 	: STD_LOGIC_VECTOR( 9 DOWNTO 0 );
+	SIGNAL DecodePC_plus_4 	: STD_LOGIC_VECTOR( 9 DOWNTO 0 );
+	SIGNAL ExecutePC_plus_4 : STD_LOGIC_VECTOR( 9 DOWNTO 0 );
 	SIGNAL read_data_1 		: STD_LOGIC_VECTOR( 31 DOWNTO 0 );
 	SIGNAL read_data_2 		: STD_LOGIC_VECTOR( 31 DOWNTO 0 );
 	SIGNAL Sign_Extend 		: STD_LOGIC_VECTOR( 31 DOWNTO 0 );
@@ -62,18 +79,17 @@ ARCHITECTURE structure OF MIPS IS
 	SIGNAL RegDst 			: STD_LOGIC_VECTOR( 1 DOWNTO 0 );
 	SIGNAL Regwrite 		: STD_LOGIC;
 	SIGNAL isBranchConditionTrue: STD_LOGIC;
-	SIGNAL Mul 				: STD_LOGIC; -- execute multiply
-	SIGNAL jal_c 			: STD_LOGIC; -- execute multiply
 	SIGNAL Jump 			: STD_LOGIC;
 	SIGNAL Jr_ctl 			: STD_LOGIC;
 	SIGNAL Jr_Address    	: STD_LOGIC_VECTOR( 7 DOWNTO 0 );
 	SIGNAL MemWrite 		: STD_LOGIC;
 	SIGNAL MemWrite_4memory	: STD_LOGIC;  -- MemWrite AND not peripherial writing 
-	SIGNAL MemRead_4memory	: STD_LOGIC;  -- MemRead AND not peripherial writing 
+	SIGNAL MemRead_4memory	: STD_LOGIC;  -- MemRead  AND not peripherial writing 
 	SIGNAL MemtoReg 		: STD_LOGIC;
 	SIGNAL MemRead 			: STD_LOGIC;
 	SIGNAL ALUoperation 	: STD_LOGIC_VECTOR( 4 DOWNTO 0 );
-	SIGNAL Instruction		: STD_LOGIC_VECTOR( 31 DOWNTO 0 );
+	SIGNAL FetchInstruction : STD_LOGIC_VECTOR( 31 DOWNTO 0 );
+	SIGNAL DecodeInstruction: STD_LOGIC_VECTOR( 31 DOWNTO 0 );
 	SIGNAL Peri_address		: STD_LOGIC_VECTOR( 3 DOWNTO 0 ); --[A11,A4,A3,A2]
 	SIGNAL Data_Bus			: STD_LOGIC_VECTOR( 31 DOWNTO 0 );
 	signal real_reset 		: STD_LOGIC;
@@ -84,33 +100,40 @@ BEGIN
 -----------------------------------------------
 					-- copy important signals to output pins for easy 
 					-- display in Simulator
-   Instruction_out 	<= Instruction;
+   Instruction_out 	<= FetchInstruction;
    ALU_result_out 	<= ALU_result;
    read_data_1_out 	<= read_data_1;
    read_data_2_out 	<= read_data_2;
-   write_data_out  	<= read_data WHEN MemtoReg = '1' ELSE ALU_result;--"11111111111111111111111111111111";
    Branch_out 		<= Branch;
-   isBranchConditionTrue_out 		<= isBranchConditionTrue;
+   isBranchConditionTrue_out <= isBranchConditionTrue;
    RegWrite_out 	<= RegWrite;
    MemWrite_out 	<= MemWrite;	
    Peri_address 	<= ALU_Result(11)&ALU_Result (4 DOWNTO 2); -- peripherial address
    real_reset		<= reset;
    MemWrite_4memory <= MemWrite and not(Peri_address(3)); -- Peri_address(3) = Address(11) means peripherial writing
    MemRead_4memory  <= MemRead and not(Peri_address(3)); -- Peri_address(3) = Address(11) means peripherial writing
-  
-					-- DATA BUS tristates
-	DATA_BUS 		<= read_data_2 WHEN MemWrite = '1' 		  ELSE "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ"; -- tristate write to memory with data_bus 
-	DATA_BUS		<= read_data   WHEN MemRead_4memory = '1' ELSE "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ"; -- tristate read from memory with data_bus
+-----------------------------------------------
+-- mux: reading data from memory/ALU (*unused signal*)
+-----------------------------------------------		
+	write_data_out  	<= read_data WHEN MemtoReg = '1' ELSE ALU_result;
+-----------------------------------------------
+-- DATA BUS tristates
+-----------------------------------------------
+					
+					-- tristate write to memory with data_bus from register R[Rb]
+	DATA_BUS 		<= read_data_2 WHEN MemWrite = '1' 		  ELSE "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
+
+					-- tristate read from memory with data_bus 
+	DATA_BUS		<= read_data   WHEN MemRead_4memory = '1' ELSE "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ"; 
 	
    
- -----------------------------------------------
+-----------------------------------------------
 -- instanciations
 -----------------------------------------------  
-					-- connect the 5 MIPS components   
 
-  IFE : Ifetch
-	PORT MAP (	Instruction 	=> Instruction,
-    	    	PC_plus_4_out 	=> PC_plus_4,
+   IFE : Ifetch
+	PORT MAP (	Instruction 	=> FetchInstruction,
+    	    	PC_plus_4_out 	=> FetchPC_plus_4,
 				Add_result 		=> Add_result,
 				Branch 			=> Branch,
 				Jump     		=> Jump,
@@ -121,23 +144,31 @@ BEGIN
 				clock 			=> CLKout,  
 				reset 			=> real_reset );
 
+   FirstPipeInst: firstpipe
+   PORT MAP (	Instruction 		=> FetchInstruction,
+   				PCplus4 			=> FetchPC_plus_4,
+				delayedInstruction	=> DecodeInstruction,
+				delayedPCplus4		=> DecodePC_plus_4 --will be used at execute
+   );
+
    ID : Idecode
-   	PORT MAP (	read_data_1 	=> read_data_1,
-        		read_data_2 	=> read_data_2,
-        		Instruction 	=> Instruction,
-        		read_data 		=> DATA_BUS,
+   	PORT MAP (	read_data_1 	=> read_data_1,-- data from register R[Ra]
+        		read_data_2 	=> read_data_2,-- data from register R[Rb]
+        		Instruction 	=> DecodeInstruction,
+        		read_data 		=> DATA_BUS,   -- data for writing to register file
 				ALU_result 		=> ALU_result,
 				RegWrite 		=> RegWrite,
 				MemtoReg 		=> MemtoReg,
 				RegDst 			=> RegDst,
 				Sign_extend 	=> Sign_extend,
         		clock 			=> CLKout,  
-				reset 			=> real_reset );
+				reset 			=> real_reset 
+	);
 
 
    CTL:   control
-	PORT MAP ( 	Opcode 			=> Instruction( 31 DOWNTO 26 ),
-				Function_opcode => Instruction( 5 DOWNTO 0 ),
+	PORT MAP ( 	Opcode 			=> DecodeInstruction( 31 DOWNTO 26 ),
+				Function_opcode => DecodeInstruction( 5 DOWNTO 0 ),
 				RegDst 			=> RegDst,
 				ALUSrc 			=> ALUSrc,
 				MemtoReg 		=> MemtoReg,
@@ -161,7 +192,7 @@ BEGIN
 				Jr_Address		=> Jr_Address,
                 ALUresult		=> ALU_Result,
 				branchAddressResult	  => Add_Result,
-				PC_plus_4		=> PC_plus_4,
+				PC_plus_4		=> ExecutePC_plus_4,
                 Clock			=> CLKout,
 				Reset			=> real_reset );
 
