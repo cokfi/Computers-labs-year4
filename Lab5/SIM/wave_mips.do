bc39b2e596ed5c4b7471685778b48b391c02cbd3
cokfi <74899649+cokfi@users.noreply.github.com>
2022-06-27 14:12:15 +0300

    !!single cycle is working!!

---
+++ Lab5/SIM/wave_mips.do
@@ -0,0 +1,99 @@
+onerror {resume}
+quietly WaveActivateNextPane {} 0
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/IFE/clock
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/IFE/reset
+add wave -noupdate -divider Fetch
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/IFE/Instruction
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/IFE/PC_plus_4_out
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/IFE/Add_result
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/IFE/Branch
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/IFE/PC_out
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/IFE/PC
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/IFE/PC_plus_4
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/IFE/next_PC
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/IFE/Mem_Addr
+add wave -noupdate -divider Decode
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/read_data_1
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/read_data_2
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/Instruction
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/read_data
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/ALU_result
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/RegWrite
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/MemtoReg
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/RegDst
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/Sign_extend
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/clock
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/reset
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/register_array
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/write_register_address
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/write_data
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/read_register_1_address
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/read_register_2_address
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/write_register_address_1
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/write_register_address_0
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/ID/Instruction_immediate_value
+add wave -noupdate -divider Execute
+add wave -noupdate /mips_tb/U_0/EXE/ALUoperation
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/EXE/Read_data_1
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/EXE/Read_data_2
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/EXE/Sign_extend
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/EXE/Function_opcode
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/EXE/ALUSrc
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/EXE/PC_plus_4
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/EXE/clock
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/EXE/reset
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/EXE/Ainput
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/EXE/Binput
+add wave -noupdate /mips_tb/U_0/EXE/branchAddressResult
+add wave -noupdate /mips_tb/U_0/EXE/isBranchConditionTrue
+add wave -noupdate /mips_tb/U_0/EXE/ALUpreliminaryOutput
+add wave -noupdate /mips_tb/U_0/EXE/ALUresult
+add wave -noupdate -divider Mem
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/MEM/read_data
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/MEM/address
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/MEM/write_data
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/MEM/MemRead
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/MEM/Memwrite
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/MEM/clock
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/MEM/reset
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/MEM/write_clock
+add wave -noupdate -divider {Control Signals}
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/CTL/Opcode
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/CTL/RegDst
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/CTL/ALUSrc
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/CTL/MemtoReg
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/CTL/RegWrite
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/CTL/MemRead
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/CTL/MemWrite
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/CTL/Branch
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/CTL/Lw
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/CTL/Sw
+add wave -noupdate -radix hexadecimal /mips_tb/U_0/CTL/Beq
+add wave -noupdate -divider graphics
+add wave -noupdate /mips_tb/HEX0
+add wave -noupdate /mips_tb/HEX1
+add wave -noupdate /mips_tb/HEX2
+add wave -noupdate /mips_tb/HEX3
+add wave -noupdate /mips_tb/LEDG
+add wave -noupdate /mips_tb/LEDR
+add wave -noupdate /mips_tb/SW
+add wave -noupdate -divider data_bus
+add wave -noupdate -radix decimal /mips_tb/U_0/PERI/Data
+TreeUpdate [SetDefaultTree]
+WaveRestoreCursors {{Cursor 1} {272450000 ps} 0}
+quietly wave cursor active 1
+configure wave -namecolwidth 189
+configure wave -valuecolwidth 62
+configure wave -justifyvalue left
+configure wave -signalnamewidth 0
+configure wave -snapdistance 10
+configure wave -datasetprefix 0
+configure wave -rowmargin 4
+configure wave -childrowmargin 2
+configure wave -gridoffset 0
+configure wave -gridperiod 1
+configure wave -griddelta 40
+configure wave -timeline 0
+configure wave -timelineunits ps
+update
+WaveRestoreZoom {246093750 ps} {278906250 ps}
--- Lab5/VHDL/aux_package.vhd
+++ Lab5/VHDL/aux_package.vhd
@@ -2,22 +2,28 @@ library IEEE;
 use ieee.std_logic_1164.all;
 
 package aux_package is
-----------------------------top-------------------------------
-	component top IS
-	GENERIC(n : INTEGER := 8;
-			k : integer := 3;    -- k=log2(n)
-			m : integer := 4);	 -- m=2^(k-1)
-	PORT (  clk, rst									: 	IN  STD_LOGIC;
-			switches 									: 	IN	STD_LOGIC_VECTOR (n-1 DOWNTO 0);
-			enableYkey0, enableALUFNkey1, enableXkey2	:	IN 	STD_LOGIC;
-			XvectorHex0,XvectorHex1						: 	OUT STD_LOGIC_VECTOR (0 to 6);
-			YvectorHex2,YvectorHex3						: 	OUT STD_LOGIC_VECTOR (0 to 6);
-			ALUFNredLeds9to5 							: 	OUT STD_LOGIC_VECTOR (4 DOWNTO 0); -- ALU function
-			ALUoutGreenLeds								: 	OUT STD_LOGIC_VECTOR(n-1 downto 0);
-			NflagRedLed0,CflagRedLed1,ZflagRedLed2		: 	OUT STD_LOGIC ); -- Zflag,Cflag,Nflag
-  	END component;
+----------------------------MIPS-------------------------------
+component MIPS IS
+
+	PORT( reset, clock					: IN 	STD_LOGIC; -- reset is opposite
+		-- Output important signals to pins for easy display in Simulator
+		PC								: OUT  STD_LOGIC_VECTOR( 9 DOWNTO 0 );
+		ALU_result_out, read_data_1_out,
+		read_data_2_out, write_data_out,	
+     	Instruction_out					: OUT 	STD_LOGIC_VECTOR( 31 DOWNTO 0 );
+		Branch_out, isBranchConditionTrue_out,
+		Memwrite_out, Regwrite_out		: OUT 	STD_LOGIC;
+		LEDG				: OUT 	STD_LOGIC_VECTOR( 7 DOWNTO 0 ); --Green leds
+		LEDR				: OUT 	STD_LOGIC_VECTOR( 7 DOWNTO 0 ); --Red leds
+		HEX0				: OUT 	STD_LOGIC_VECTOR( 0 TO 6); 
+		HEX1				: OUT 	STD_LOGIC_VECTOR( 0 TO 6); 
+		HEX2				: OUT 	STD_LOGIC_VECTOR( 0 TO 6); 
+		HEX3				: OUT 	STD_LOGIC_VECTOR( 0 TO 6); 
+		SW					: IN 	STD_LOGIC_VECTOR( 7 DOWNTO 0 ) -- Switches		
+		);
+END 	component;
 ----------------------------control-------------------------------	
-ENTITY control IS
+component control IS
    PORT( 	
 	Opcode 			: IN 	STD_LOGIC_VECTOR( 5 DOWNTO 0 );
 	Function_opcode : IN 	STD_LOGIC_VECTOR( 5 DOWNTO 0 );
@@ -28,18 +34,18 @@ ENTITY control IS
 	MemRead 		: OUT 	STD_LOGIC;
 	MemWrite 		: OUT 	STD_LOGIC;
 	Branch 			: OUT 	STD_LOGIC;
-	ALUoperation 	: OUT 	STD_LOGIC_VECTOR( 3 DOWNTO 0 );
-	Jump			: OUT 	STD_LOGIC;
+	ALUoperation 	: OUT 	STD_LOGIC_VECTOR( 4 DOWNTO 0 );
+	Jump			: OUT 	STD_LOGIC
 	);
 
 END component;
 ----------------------------execute-------------------------------
-ENTITY  execute IS
+component  execute IS
 	PORT(	Read_data_1 			: IN 	STD_LOGIC_VECTOR( 31 DOWNTO 0 ); -- data from register R[Ra]
 			Read_data_2 			: IN 	STD_LOGIC_VECTOR( 31 DOWNTO 0 ); -- data from register R[Rb]
 			Sign_extend 			: IN 	STD_LOGIC_VECTOR( 31 DOWNTO 0 );
 			Function_opcode 		: IN 	STD_LOGIC_VECTOR( 5 DOWNTO 0 );
-			ALUoperation  			: IN 	STD_LOGIC_VECTOR( 2 DOWNTO 0 ); -- selector from control unit 
+			ALUoperation  			: IN 	STD_LOGIC_VECTOR( 4 DOWNTO 0 ); -- selector from control unit 
 			ALUSrc 					: IN 	STD_LOGIC; 						-- selector from control unit
 			PC_plus_4 				: IN 	STD_LOGIC_VECTOR( 9 DOWNTO 0 );
 			clock, reset			: IN 	STD_LOGIC; 
@@ -50,5 +56,64 @@ ENTITY  execute IS
 			branchAddressResult 	: OUT	STD_LOGIC_VECTOR( 7 DOWNTO 0 ) 	-- address result for branch 
 			);
 END component;	
+----------------------------Idecode-------------------------------
+component Idecode IS
+	  PORT(	read_data_1	: OUT 	STD_LOGIC_VECTOR( 31 DOWNTO 0 );
+			read_data_2	: OUT 	STD_LOGIC_VECTOR( 31 DOWNTO 0 );
+			Instruction : IN 	STD_LOGIC_VECTOR( 31 DOWNTO 0 );
+			read_data 	: IN 	STD_LOGIC_VECTOR( 31 DOWNTO 0 );
+			ALU_result	: IN 	STD_LOGIC_VECTOR( 31 DOWNTO 0 );
+			RegWrite 	: IN 	STD_LOGIC;
+			MemtoReg 	: IN 	STD_LOGIC;
+			RegDst 		: IN 	STD_LOGIC_VECTOR( 1 DOWNTO 0 );
+			Sign_extend : OUT 	STD_LOGIC_VECTOR( 31 DOWNTO 0 );
+			clock,reset	: IN 	STD_LOGIC );
+END component;
+----------------------------Ifetch-------------------------------
+component Ifetch IS
+	PORT(	
+			SIGNAL clock, reset 	: IN 	STD_LOGIC;
+			SIGNAL Add_result 		: IN 	STD_LOGIC_VECTOR( 7 DOWNTO 0 ); -- branch address result
+        	SIGNAL Branch 			: IN 	STD_LOGIC;
+        	SIGNAL isBranchConditionTrue : IN 	STD_LOGIC;
+			SIGNAL Jump 			: IN 	STD_LOGIC;
+			SIGNAL Jr	 			: IN 	STD_LOGIC;
+			SIGNAL Jr_Address		: IN	STD_LOGIC_VECTOR( 7 DOWNTO 0 );
+			SIGNAL Instruction 		: OUT	STD_LOGIC_VECTOR( 31 DOWNTO 0 );
+        	SIGNAL PC_plus_4_out 	: OUT	STD_LOGIC_VECTOR( 9 DOWNTO 0 );
+      		SIGNAL PC_out 			: OUT	STD_LOGIC_VECTOR( 9 DOWNTO 0 )
+        	);
+END component;
+----------------------------Dmemory-------------------------------
+component dmemory IS
+	PORT(	read_data 			: OUT 	STD_LOGIC_VECTOR( 31 DOWNTO 0 );
+        	address 			: IN 	STD_LOGIC_VECTOR( 7 DOWNTO 0 );
+        	write_data 			: IN 	STD_LOGIC_VECTOR( 31 DOWNTO 0 );
+	   		MemRead, Memwrite 	: IN 	STD_LOGIC;
+            clock,reset			: IN 	STD_LOGIC );
+END component;
+----------------------------peripherial-------------------------------
+component peripherial IS
+	PORT(reset				: IN 	STD_LOGIC;
+		 clock			    : IN 	STD_LOGIC;
+		 MemRead 			: IN 	STD_LOGIC;
+         MemWrite 			: IN 	STD_LOGIC;
+		 Address			: IN	STD_LOGIC_VECTOR( 3 DOWNTO 0 ); --[A11,A4,A3,A2]
+		 Data				: INOUT	STD_LOGIC_VECTOR( 7 DOWNTO 0 );	--[D7,...,D0]
+		 LEDG				: OUT 	STD_LOGIC_VECTOR( 7 DOWNTO 0 ); --Green leds
+		 LEDR				: OUT 	STD_LOGIC_VECTOR( 7 DOWNTO 0 ); --Red leds
+		 HEX0				: OUT 	STD_LOGIC_VECTOR( 0 TO 6 ); 
+		 HEX1				: OUT 	STD_LOGIC_VECTOR( 0 TO 6 );  
+		 HEX2				: OUT 	STD_LOGIC_VECTOR( 0 TO 6 ); 
+		 HEX3				: OUT 	STD_LOGIC_VECTOR( 0 TO 6 ); 
+		 SW					: IN 	STD_LOGIC_VECTOR( 7 DOWNTO 0 ) -- Switches
+	);
+END component;
+----------------------------divider-------------------------------
+component divider is port (
+	CLKin : in std_logic;	 
+	CLKout : out std_logic );
+end component;
+
 end aux_package;
 
